822b75682476cc14d62f3b5dca7500a4e8037a76
Sandor Suszta <sasha.shusta@gmail.com>
2023-05-23 21:07:38 +0100

    Finish project

--- Timer/TimerViewController.swift
+++ Timer/TimerViewController.swift
@@ -6,7 +6,13 @@ final class ViewController: UIViewController {
     
     private var timerState: TimerState = .pickingTime
     
-    private let formatter: DateComponentsFormatter = {
+    private var remainingTime: Int = 0 {
+        didSet {
+            timeLabel.text = timeFormatter.string(from: TimeInterval(remainingTime))
+        }
+    }
+    
+    private let timeFormatter: DateComponentsFormatter = {
         let formatter = DateComponentsFormatter()
         formatter.allowedUnits = [.hour, .minute, .second]
         return formatter
@@ -27,18 +33,20 @@ final class ViewController: UIViewController {
     
     private lazy var startPauseButton: UIButton = {
         let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Start", for: .normal)
         button.setTitleColor(.systemBlue, for: .normal)
+        button.backgroundColor = .systemBlue.withAlphaComponent(0.1)
+        button.layer.cornerRadius = 16
         button.addTarget(self, action: #selector(startResumeButtonTapped), for: .touchUpInside)
         return button
     }()
     
     private lazy var stopButton: UIButton = {
         let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Stop", for: .normal)
         button.setTitleColor(.systemRed, for: .normal)
+        button.backgroundColor = .systemRed.withAlphaComponent(0.1)
+        button.layer.cornerRadius = 16
         button.addTarget(self, action: #selector(stopButtonTapped), for: .touchUpInside)
         return button
     }()
@@ -60,8 +68,9 @@ final class ViewController: UIViewController {
         case .pickingTime:
             timerState = .isRunning
             timePicker.isHidden = true
-            timeLabel.text = formatter.string(from: timePicker.countDownDuration)
             timeLabel.isHidden = false
+            remainingTime = Int(timePicker.countDownDuration)
+            
             startTimer()
             
         case .isRunning:
@@ -82,10 +91,28 @@ final class ViewController: UIViewController {
     }
     
     private func startTimer() {
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+            
+            guard let self,
+                  self.remainingTime > 0
+            else {
+                return
+            }
+            
+            self.remainingTime -= 1
+        }
+        
+        if let timer {
+            RunLoop.current.add(timer, forMode: RunLoop.Mode.common)
+        }
+        
         startPauseButton.setTitle("Pause", for: .normal)
     }
     
     private func pauseTimer() {
+        timer?.invalidate()
+        timer = nil
         startPauseButton.setTitle("Resume", for: .normal)
     }
     
@@ -107,7 +134,6 @@ final class ViewController: UIViewController {
     }
     
     private func setupLayout() {
-        
         timeLabel.translatesAutoresizingMaskIntoConstraints = false
         startPauseButton.translatesAutoresizingMaskIntoConstraints = false
         stopButton.translatesAutoresizingMaskIntoConstraints = false
@@ -122,9 +148,13 @@ final class ViewController: UIViewController {
             
             stopButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
             stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stopButton.widthAnchor.constraint(equalToConstant: 150),
+            stopButton.heightAnchor.constraint(equalToConstant: 44),
             
-            startPauseButton.bottomAnchor.constraint(equalTo: stopButton.topAnchor, constant: -30),
+            startPauseButton.bottomAnchor.constraint(equalTo: stopButton.topAnchor, constant: -16),
             startPauseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startPauseButton.widthAnchor.constraint(equalToConstant: 150),
+            startPauseButton.heightAnchor.constraint(equalToConstant: 44)
         ])
     }
 }
